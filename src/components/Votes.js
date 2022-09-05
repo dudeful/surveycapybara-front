87572e37f9c5d7ec81ce666313a0a267578c6174
Fabricio Rodrigues Pereira <fabri.rp2010@gmail.com>
2022-09-05 13:50:32 -0300

    viaul up

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -12,10 +12,10 @@ export function Votes (props){
     };
     return (
         <div>
-            <button id={"option_1"} onClick={onClick}>Opção a {props.state["option_1"]}</button>
-            <button id={"option_2"} onClick={onClick}>Opção b {props.state["option_2"]}</button>
-            <button id={"option_3"} onClick={onClick}>Opção c {props.state["option_3"]}</button>
-            <button id={"option_4"} onClick={onClick}>Opção d {props.state["option_4"]}</button>
+            <button id={"option_1"} onClick={onClick}>Opção a | {props.state["option_1"]}</button>
+            <button id={"option_2"} onClick={onClick}>Opção b | {props.state["option_2"]}</button>
+            <button id={"option_3"} onClick={onClick}>Opção c | {props.state["option_3"]}</button>
+            <button id={"option_4"} onClick={onClick}>Opção d | {props.state["option_4"]}</button>
             <input value={props.state["total"]}  disabled={true} />
         </div>
     )
