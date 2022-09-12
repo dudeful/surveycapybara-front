2af9a927155fe3c8d8524a55762bcbe2a028da30
danubiomuller <dudeful@outlook.com>
2022-09-11 23:12:17 -0300

    add type button to prevent reload

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -3,11 +3,11 @@ import React, { useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { UserContext } from '../Context/UserContext';
 import Header from '../Header/Header';
-import IconProfile from '../../img/icon_profile.png'
-import IconProfilePlus from '../../img/icon_profile_plus.png'
-import IconEmail from '../../img/icon_email.png'
-import IconPassword from '../../img/icon_password.png'
-import IconLogin from '../../img/icon_login.png'
+import IconProfile from '../../img/icon_profile.png';
+import IconProfilePlus from '../../img/icon_profile_plus.png';
+import IconEmail from '../../img/icon_email.png';
+import IconPassword from '../../img/icon_password.png';
+import IconLogin from '../../img/icon_login.png';
 
 const API_URL = 'https://server-surveycapybara.dudeful.com';
 // const LOCALHOST = 'http://localhost:5000';
@@ -45,36 +45,48 @@ function Login(props) {
 
   return (
     <>
-      <Header>  </Header>
+      <Header> </Header>
       <form id="login">
-
         <fieldset>
-          <legend className='box-login-modal'>
-            <div className='box-modal-login'>
+          <legend className="box-login-modal">
+            <div className="box-modal-login">
               <img src={IconProfile} alt="" />
-              <input className='input-btn-login' type="button" value="LOGIN" />
+              <input className="input-btn-login" type="button" value="LOGIN" />
             </div>
 
-            <div className='box-modal-register'>
+            <div className="box-modal-register">
               <img src={IconProfilePlus} alt="" />
-              <input className='input-btn-register' type="button" value="REGISTER" />
+              <input className="input-btn-register" type="button" value="REGISTER" />
             </div>
           </legend>
-          <div className='input-box-email'>
+          <div className="input-box-email">
             <img src={IconEmail} alt="" />
-            <input className='input-email' placeholder="Your  email" type="email" id="login_email" />
+            <input
+              className="input-email"
+              placeholder="Your  email"
+              type="email"
+              id="login_email"
+            />
           </div>
-          <div className='input-box-password'>
+          <div className="input-box-password">
             <img src={IconPassword} alt="" />
-            <input className='input-password' placeholder="Your password" type="password" id="login_password" />
+            <input
+              className="input-password"
+              placeholder="Your password"
+              type="password"
+              id="login_password"
+            />
           </div>
-          <div ><a href="http://#" target="_blank" rel="noopener noreferrer">Forgot Password?</a></div>
+          <div>
+            <a href="http://#" target="_blank" rel="noopener noreferrer">
+              Forgot Password?
+            </a>
+          </div>
 
-          <button className='btn-login' id="login_button" onClick={loginHandler}>
+          <button type="button" className="btn-login" id="login_button" onClick={loginHandler}>
             <h2>LOGIN</h2>
             <img src={IconLogin} alt="" />
           </button>
-
         </fieldset>
       </form>
       {/* <div id="login">
