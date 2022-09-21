0ac064d00893f070fed18518e83d5e1ee6536f54
Fabricio Rodrigues Pereira <fabri.rp2010@gmail.com>
2022-09-21 14:15:09 -0300

    fix: bad css

--- src/components/CreatePool/index.jsx
+++ src/components/CreatePool/index.jsx
@@ -1,29 +1,74 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import "./styles.css"
 
 function CreatePool(props){
-    const inputArr = [{
+    const navigate = useNavigate();
+    const [prot, setProt] = useState(false);
+    const [arr, setArr] = useState([{
         value: "",
-        id: "option-0",
-        key: 0
+        id: "option-0"
     },
     {
         value: "",
-        id: "option-2",
-        key: 1
-    }
-    ]
-
-    const [arr, setArr] = useState(inputArr);
-
+        id: "option-1"
+    }]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(event);
+        const pool = {
+            owner: "dudeful@outlook.com",
+            name: document.getElementById("title").value,
+            description: "Testes",
+            positive_votes_per_voter: document.getElementById("numVotes").value,
+            negative_votes_per_voter: 0,
+            negative_votes_threshold: 0,
+            weighted_vote: false,
+            visible_vote: true,
+            private_pool: prot,
+            pool_password: null,
+            voting_time: 1000,
+            registered_pool: false,
+            open_options: false,
+            options_per_voter: 0,
+            options: arr
+        };
+
+        const complete = arr.reduce((previusValor, currentValor) => {
+            if ((currentValor.value !== "") && (previusValor)){
+                return true
+            }else{
+                return false;
+            }
+        }, true);
+
+        if (complete){
+            if (prot){
+                const pass = document.getElementById("poolpass");
+                if (pass !== "" && pool.name !== ""){
+                    pool.pool_password = pass.value;
+                }else{
+                    return;
+                }
+            }
+        }else{
+            return;
+        }
+
+        fetch('https://server-surveycapybara.dudeful.com/pools/new', {
+            method: 'POST',
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({pool})
+        });
+
+        navigate(`/code`);
     };
     const handleButton = (event) => {
         event.preventDefault();
-        setArr(s => [...s,{value: ""}]);
+        setArr(s => [...s,{
+            value: "",
+            id: "option-"+arr.length
+        }]);
     }
     const handleNumber = (event) => {
         event.preventDefault();
@@ -33,20 +78,51 @@ function CreatePool(props){
             event.target.value = arr.length-1;
         }
     }
+
+    const inputHandler = (event) =>{
+        arr[Number(event.target.id.split("-")[1])].value=event.target.value;
+    }
+
+    const checkboxHandler = ()=>{
+        if (prot){
+            setProt(false);
+        }else{
+            setProt(true);
+        }
+    }
+
+    const password = () =>{
+        if (prot){
+            return (
+            <div className='input-form-box'>
+                <input id="poolpass" type="password" className={"input-form"} placeholder="Senha" />
+            </div>);
+        }else{
+            return "";
+        }
+    }
+
     return (
     <div className="create-pool">
+        <h2 className='asap'>Crie Sua Pesquisa</h2>
         <fieldset className='box-fieldset-create-pool'>
-            <h2 className='asap'>Crie Sua Pesquisa</h2>
-            <form id="create-pool box-modal" onSubmit={handleSubmit} >
+            <form id="create-pool" onSubmit={handleSubmit} >
                 <div className='input-form-box'>
-                    <input type="text" className={"input-form"} placeholder="Titulo da Pesquisa" />
+                    <input id="title" type="text" className={"input-form"} placeholder="Titulo da Pesquisa" />
                 </div>
                 <div className='input-form-box input-form-box-label'>
                     <label className='label-input' htmlFor="numVotes">
                         N° de votos:
                     </label>
-                    <input name="numVotes" type="number" className={"input-form input-form-small"} onChange={handleNumber} defaultValue={1} />
+                    <input id="numVotes" name="numVotes" type="number" className={"input-form input-form-small"} onChange={handleNumber} defaultValue={1} />
                 </div>
+                <div className='input-form-box input-form-box-label'>
+                    <label className='label-input' htmlFor="secury">
+                        Proteger com senha: 
+                    </label>
+                    <input type="checkbox" name="secury" onChange={checkboxHandler} />
+                </div>
+                {password()}
                 {arr.map((item, i) => {
                     return (
                         <div className='input-form-box'>
@@ -54,16 +130,16 @@ function CreatePool(props){
                                 defaultValue={item.value}
                                 id={"option-"+i}
                                 type={"text"}
-                                key={i}
                                 className={"input-form"}
                                 placeholder={"Preencher opção"}
+                                onChange={inputHandler}
                             />
                         </div>
                         );
                 })}
                 <div className='divButtons'>
                     <button className="bnt-forms asap" onClick={handleButton}><strong>Adicionar Opção</strong></button>
-                    <input className="bnt-forms asap" type="submit" value={"Criar"} />
+                    <input className="bnt-forms asap" type="submit" value={"Criar"} onClick={handleSubmit} />
                 </div>
             </form>
         </fieldset>
