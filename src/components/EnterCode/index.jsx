7ecfd0b4a2446a29750c73bab5bcfbd8865dee1d
Fabricio Rodrigues Pereira <fabri.rp2010@gmail.com>
2022-09-22 13:57:28 -0300

    enchencement: partion search integration

--- src/components/EnterCode/index.jsx
+++ src/components/EnterCode/index.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import Header from '../Header/Header';
 import SideBar from '../SideBar';
+import { useNavigate } from 'react-router-dom';
 import "./styles.css"
 
 const EnterCode = () => {
+    const navigate = useNavigate();
     const buttonHandler = (event)=>{
+        event.preventDefault();
+        const pool = {
+            id: document.getElementById("codeField").value,
+            password: document.getElementById("password").value
+        }
 
+        if (pool.id === ""){
+            console.log("vazio");
+            return;
+        }
+
+        const url = '/pool/' + pool.id + ((pool.password !== "") ? '?' + pool.password : "");
+
+        try {
+            //fetch(url).then((response) => console.log(response.json())).then((data) => console.log(data));
+            navigate(url);
+        } catch (error) {
+            console.log(error);
+        }
     }
     return (
         <>
@@ -15,8 +35,8 @@ const EnterCode = () => {
                 <form className='box-form-code centrilize'>
                     <h2> Surveycabybara</h2>
                     <fieldset className='box-fieldset-code'>
-                        <input className='input-code-enter' type="text" placeholder='Enter code' />
-                        <input className='input-code-password' type="password" placeholder='Enter password' />
+                        <input id='codeField' className='input-code-enter' type="text" placeholder='Enter code' />
+                        <input id='password' className='input-code-password' type="password" placeholder='Enter password' />
                         <input className='input-code-btn' type="button" value="Enter" onClick={buttonHandler} />
                     </fieldset>
                 </form>
