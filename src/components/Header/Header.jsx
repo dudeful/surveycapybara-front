395ef123c57a785089b17e8423d76cb32d1d4122
Alehff <alehffeinstein@gmail.com>
2022-09-26 13:22:23 -0300

    fixing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logoCapybara from "../../img/capybara.svg"
+import logoCapybara from '../../img/capybara.svg';
 const Header = (props) => {
-  const profile = props.profile
+  const profile = props.profile;
   return (
-    <header >
-     <img  className='img-logo-capybara' src={logoCapybara} alt="" />
+    <header>
+      <img className="img-logo-capybara" src={logoCapybara} alt="" />
 
-      <div className='box-header-n'>
-        <h4 className='profile-or-login'>{profile ? profile : "login"}</h4>
-        <h4 className='sign-up-or-in'>{profile === "anonymous" ? "sign-up" : "sign-in"}</h4>
+      <div className="box-header-n">
+        <h4 className="profile-or-login">{profile ? profile : 'login'}</h4>
+        <h4 className="sign-up-or-in">{profile === 'qqr coida' ? 'sign-up' : 'sign-in'}</h4>
       </div>
     </header>
   );
