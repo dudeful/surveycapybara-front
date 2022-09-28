6b6d8479fd69452aa1a2d1f5d86fcd90f035dd3a
Fabricio Rodrigues Pereira <fabri.rp2010@gmail.com>
2022-09-27 22:53:26 -0300

    fix: urls ws and api

--- src/components/Env.jsx
+++ src/components/Env.jsx
@@ -4,5 +4,5 @@
 //export const API_URL = 'https://server-surveycapybara.dudeful.com';
 //export const WS_URL = 'wss://ws-server-surveycapybara.dudeful.com';
 
-export const API_URL = 'https://https://45.77.153.6:5000/';
-export const WS_URL = 'wss://https://45.77.153.6:5050/';
+export const API_URL = 'http://45.77.153.6:5000/';
+export const WS_URL = 'wss://45.77.153.6:5050/';
