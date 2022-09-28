2bc9a0a988ff095c677befaed07db666def0bce9
Fabricio Rodrigues Pereira <fabri.rp2010@gmail.com>
2022-09-27 22:47:57 -0300

    fix: urls to profs machine

--- src/components/Env.jsx
+++ src/components/Env.jsx
@@ -1,5 +1,8 @@
 // export const API_URL = 'http://localhost:5000';
 // export const WS_URL = 'ws://localhost:5050';
 
-export const API_URL = 'https://server-surveycapybara.dudeful.com';
-export const WS_URL = 'wss://ws-server-surveycapybara.dudeful.com';
+//export const API_URL = 'https://server-surveycapybara.dudeful.com';
+//export const WS_URL = 'wss://ws-server-surveycapybara.dudeful.com';
+
+export const API_URL = 'https://https://45.77.153.6:500/';
+export const WS_URL = 'wss://https://45.77.153.6:5050/';
